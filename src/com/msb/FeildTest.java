26547d3169538ee96024620605aa4ffa1d235ce7
liudashuaiqi <54491357+liudashuaiqi@users.noreply.github.com>
2020-10-04 14:37:03 +0800

    12312

--- src/com/msb/FeildTest.java
+++ src/com/msb/FeildTest.java
@@ -1,10 +1,31 @@
 package com.msb;
 
+import java.util.Objects;
+
 public class FeildTest {
+    int a ;
     public static void main(String[] args) {
+        //22.00
         System.out.println(1);
         System.out.println(2);
         int b;
         A a = new A();
+        System.out.println(a.hashCode());
+        A sss = a;
+        System.out.println(sss.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeildTest feildTest = (FeildTest) o;
+        return a == feildTest.a;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(a);
     }
 }
---
+++ src/com/msb/Solution.java
@@ -0,0 +1,7 @@
+package com.msb;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
